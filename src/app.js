da505b6c5ad19d8e46f7a39ef77344e9407dbf42
HelloMandu <tjdals6695@gmail.com>
2021-10-10 22:51:44 +0900

    lint, prettier 적용

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-import { sum } from './math';
-import './index.scss';
+import { sum } from "./math";
+import "./index.scss";
 
 console.log(sum(1, 2));
-console.log(process.env.NODE_ENV);
-console.log(VERSION);
-console.log(PRODUCTION);
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+console.log(sum(1, 2));
+// console.log(process.env.NODE_ENV);
+// console.log(VERSION);
+// console.log(PRODUCTION);
 
 new Promise();
