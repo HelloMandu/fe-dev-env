f95f8e56020d8874c886b47dccd5ca671c088568
HelloMandu <tjdals6695@gmail.com>
2021-10-10 20:00:00 +0900

    webpack plugin 적용

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ import { sum } from './math';
 import './index.css';
 
 console.log(sum(1, 2));
+console.log(process.env.NODE_ENV);
+console.log(VERSION);
+console.log(PRODUCTION);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   mode: "development",
@@ -11,14 +16,41 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js/,
-        use: path.resolve('./my-webpack-loader.js')
-      },
+      // {
+      //   test: /\.js/,
+      //   use: path.resolve('./my-webpack-loader.js')
+      // },
       {
         test: /\.css/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: () => {
+        const commit = childProcess.execSync("git rev-parse --short HEAD")
+        const user = childProcess.execSync("git config user.name")
+        const date = new Date().toLocaleString()
+        return `commitVersion: ${commit}` + `Build Date: ${date}\n` + `Author: ${user}`
+      }
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify("v.1.2.3"),
+      PRODUCTION: JSON.stringify(false)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+      },
+      minify: process.env.NODE_ENV === 'production' ? {
+        collapseWhitespace: true,
+        removeComments: true
+      } : false,
+      hash: true
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 }
